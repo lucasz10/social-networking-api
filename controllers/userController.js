9cfbff8fefbc1137c30b6145a1de6d64439561ae
lucasz10 <lucas.zach10@gmail.com>
2022-11-01 18:10:49 -0700

    Updated userController. Added routing in userRoutes

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,10 +16,48 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  // create a new user
   createUser(req, res) {
     User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+  updateUser(req, res){
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true}
+        )
+        .then((user) =>
+        !user
+        ? res.status(404).json({ message: 'No user with that ID' })
+        : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+  },
+  addFriend(req, res){
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: {friends: req.params.friendId } },
+        { runValidators: true, new: true }
+        )
+        .then((user) =>
+        !user
+        ? res.status(404).json({ message: 'No user with that ID' })
+        : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+  },
+  deleteFriend(req, res){
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: {friends: req.params.friendId } },
+        { runValidators: true, new: true }
+        )
+        .then((user) =>
+        !user
+        ? res.status(404).json({ message: 'No user with that ID' })
+        : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));    
+  }
 };
